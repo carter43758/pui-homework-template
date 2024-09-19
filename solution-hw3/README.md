271497293fac7f0f0698aae51448c6ce0a27bb2f
carter43758 <cfowen@andrew.cmu.edu>
2024-09-19 12:17:58 -0400

    modified:   .DS_Store new file:   solution-hw3/.DS_Store new file:   solution-hw3/README.md new file:   solution-hw3/cart.html new file:   solution-hw3/display.html new file:   solution-hw3/gallery.html new file:   solution-hw3/style.css new file:   solution-hw3/test.js

---
+++ solution-hw3/README.md
@@ -0,0 +1,2 @@
+Deployed website: https://carter43758.github.io/PUI-HW/solution-hw2/gallery.html
+Repository link: https://github.com/CMU-PUI-2024/pui-homework-template
---
+++ solution-hw3/test.js
@@ -0,0 +1,23 @@
+const basePrice = 2.49;
+
+const glazingOptions =[
+  {option: "Keep original", glazingPrice: 0.00},
+  {option: "Sugar milk", glazingPrice: 0.00},
+  {option: "Vanilla milk", glazingPrice: 0.50},
+  {option: "Double chocolate", glazingPrice: 1.50}
+
+];
+
+const packSizeOptions = [
+  {size: 1, packSize: 1},
+  {size: 3, packSize: 3},
+  {size: 6, packSize: 5},
+  {size: 12, packSize: 10}
+  
+];
+
+function price (basePrice, option, size) {
+const glazingPrice = glazingOptions[option].glazingPrice;
+const packMultiplier = packSizeOptions[size].packSize;
+return (basePrice + glazingPrice) * packSize;
+}
