5a162abec90d1b15a8d0c8f46c2363a5aa9d50c8
carter43758 <cfowen@andrew.cmu.edu>
2024-09-20 12:39:11 -0400

    modified:   .DS_Store modified:   solution-hw3/display.html modified:   solution-hw3/test.js

--- solution-hw3/test.js
+++ solution-hw3/test.js
@@ -1,11 +1,10 @@
 const basePrice = 2.49;
 
-const glazingOptions =[
+const glazingOptions = [
   {option: "Keep original", glazingPrice: 0.00},
   {option: "Sugar milk", glazingPrice: 0.00},
   {option: "Vanilla milk", glazingPrice: 0.50},
   {option: "Double chocolate", glazingPrice: 1.50}
-
 ];
 
 const packSizeOptions = [
@@ -13,14 +12,16 @@ const packSizeOptions = [
   {size: 3, packSize: 3},
   {size: 6, packSize: 5},
   {size: 12, packSize: 10}
-  
 ];
 
 function price (basePrice, option, size) {
-const glazingPrice = glazingOptions[option].glazingPrice;
-const packMultiplier = packSizeOptions[size].packSize;
-return (basePrice + glazingPrice) * packSize;
+    const glazingPrice = glazingOptions[option].glazingPrice;
+    const packMultiplier = packSizeOptions[size].packSize;
+    return (basePrice + glazingPrice) * packSize;
 }
 
 let selectGlaze = document.querySelector('#glazing');
 let selectPack = document.querySelector('#pack');
+
+function glazingChange(element) {
+    const priceChange = glazingPrice.value}
